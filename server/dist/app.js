3d4ac25a3b2c1c0be348d4b43021a8ded1026f2f
Nischal Bhattarai <99738369+nischaljs@users.noreply.github.com>
2025-08-21 00:15:29 +0545

    Update app.js

--- server/dist/app.js
+++ server/dist/app.js
@@ -23,6 +23,8 @@ const error_1 = require("./middleware/error");
 const allowedOrigins = [
     "http://localhost:5173",
     "https://fighter-management-system-2.onrender.com",
+    "https://fighter-management-system.vercel.app/",
+    "https://fighter-management-system.vercel.app"
 ];
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
